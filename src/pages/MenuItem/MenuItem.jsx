9b17b122d91e7ef0d6c07bf589e279eb5848d247
SantoshMule2004 <mulesantosh489@gmail.com>
2025-02-28 17:33:17 +0530

    menuitem

--- src/pages/MenuItem/MenuItem.jsx
+++ src/pages/MenuItem/MenuItem.jsx
@@ -12,32 +12,32 @@ const MenuItem = () => {
     useEffect(() => {
         // Fetch Thali items
         getMenu(itemId)
-          .then((response) => {
-            console.log("Menu Data:", response);
-            setData(response);
-          })
-          .catch((error) => console.error("Error fetching menu item:", error));
-      }, []);
+            .then((response) => {
+                console.log("Menu Data:", response);
+                setData(response);
+            })
+            .catch((error) => console.error("Error fetching menu item:", error));
+    }, []);
 
 
-    return(
+    return (
         <div className="menu-item-container">
-            <div className="menu-item-display"> 
+            <div className="menu-item-display">
                 <h1>Menu item page</h1>
                 <h2>{data.menuId}</h2>
-                
+
                 <div className="menu-item-card">
-                <img src={data.imageUrl} alt="menu image" className="menu-item-image" />
+                    <img src={data.imageUrl} alt="menu image" className="menu-item-image" />
                     <div className="menu-item-info">
-                    <h3>{data.menuName}</h3>
+                        <h3>{data.menuName}</h3>
                         <h3>{data.description}</h3>
-                       <h3>Price:</h3> <h3>{data.price}</h3>
+                        <h3>Price:</h3> <h3>{data.price}</h3>
                         <div className="menu-item-buttons">
                             <button className="menu-item-button">Add to Cart</button>
                             <button className="menu-item-button">Order Now</button>
-                    </div>
+                        </div>
 
-</div>
+                    </div>
                 </div>
             </div>
         </div>
