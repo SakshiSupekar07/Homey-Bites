e8a409a7afe988fa3fd6bb6bec82923fc334bc99
SantoshMule2004 <mulesantosh489@gmail.com>
2025-03-08 09:53:37 +0530

    updated token related issues

--- src/Services/AddToCartService.jsx
+++ src/Services/AddToCartService.jsx
@@ -1,4 +1,4 @@
-import { myAxios, myAxiosAuth } from "./Helper";
+import { myAxios } from "./Helper";
 
 export const fetchUserCart = (userId) => {
     return myAxios.get('/api/v1/cart/'+userId)
@@ -6,21 +6,21 @@ export const fetchUserCart = (userId) => {
 }
 
 export const addItemToCart = (userId, itemId) => {
-    return myAxiosAuth.post('/api/v1/cart/'+userId+'/item/'+itemId)
+    return myAxios.post('/api/v1/cart/'+userId+'/item/'+itemId)
     .then((response) => response.data);
 }
 
 export const updateQuantity = (cartId, quantity) => {
-    return myAxiosAuth.put('/api/v1/cart/'+cartId+'/quantity/'+quantity)
+    return myAxios.put('/api/v1/cart/'+cartId+'/quantity/'+quantity)
     .then((response) => response.data);
 }
 
 export const removeItemFromCart = (cartId) => {
-    return myAxiosAuth.delete('/api/v1/cart/delete/'+cartId)
+    return myAxios.delete('/api/v1/cart/delete/'+cartId)
     .then((response) => response.data);
 }
 
 export const removeAllItemFromCart = (userId) => {
-    return myAxiosAuth.delete('/api/v1/cart/'+userId)
+    return myAxios.delete('/api/v1/cart/'+userId)
     .then((response) => response.data);
 }
--- src/Services/Helper.js
+++ src/Services/Helper.js
@@ -1,23 +1,31 @@
 import axios from "axios";
 import { getAuthToken } from "../components/Auth";
 
-export const BASE_URL = 'https://homeybites.onrender.com';
-
-const token = getAuthToken();
-
+export const BASE_URL = 'https://homeybites.onrender.com'; 
+ 
 export const myAxios = axios.create({
     baseURL: BASE_URL,
 })
 
-export const myAxiosAuth = axios.create({
-  baseURL: BASE_URL,
-  withCredentials: true,
-  headers: {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": `Bearer ${token}`,
-  },
-})
+// export const myAxiosAuth = axios.create({
+//   baseURL: BASE_URL,
+//   withCredentials: true,
+//   headers: {
+//     "Accept": "application/json",
+//     "Content-Type": "application/json",
+//     "Authorization": `Bearer ${token}`,
+//   },
+// })
+
+myAxios.interceptors.request.use((config)=>{
+  const token = getAuthToken();
+  if(token && ((config.url?.includes("/api/v1/auth")) || (config.method !== "get"))) {
+      config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error)=>{
+  return Promise.reject(error);
+});
 
   // const API_BASE_URL = "https://homeybites.onrender.com ";
   // const LOGIN_API = "https://homeybites.onrender.com/api/v1/auth/login";
